0db46dae93c3769137ea746696d7c351f01273c0
Boris Quétard <u0149353@kuleuven.be>
2022-04-28 14:02:02 +0200

    bla

--- fnc_catSays.R
+++ fnc_catSays.R
@@ -1,25 +1,30 @@
 # function that make the cat say custom 
   #argument:
     #msg: the message the cat has to say. a default value is provided
-
+      #preset messages: 
+      #'tribute' (default), 'brag', 
 f_catSays = function (msg = NULL) {
   
   #default message
-  if (is.null(msg)) {
+  if (is.null(msg) | msg == 'tribute') {
     msg = 
 'This is not the best function in the world,
 this is just a tribute.'
+  }else if (msg == 'brag') {
+    msg = 
+'I am the best animal because the me function is used to print me,
+but also to print other animals!'
   }
   
   #draw the cat
-  cat = "
+  animal = "
         |/
  /.\\ /.\\\'
 (= ^.^ =) 
 (\'\') (\'\')"
   
   #make the console output with the cat function
-  cat(paste0(msg, cat))
+  cat(paste0(msg, animal))
   
 }
 
