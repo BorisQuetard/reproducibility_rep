b2416d8eda9bfaa78b33c2d1b95bbe2e18813fe4
Boris Quétard <u0149353@kuleuven.be>
2022-04-28 12:11:23 +0200

    bla

--- fnc_catSays.R
+++ fnc_catSays.R
@@ -3,18 +3,25 @@
     #msg: the message the cat has to say. a default value is provided
 
 f_catSays = function (msg = NULL) {
+  
   #default message
   if (is.null(msg)) {
-    msg = 'This is not the best function in the world,
-this is just a tribute'
+    msg = 
+'This is not the best function in the world,
+this is just a tribute.'
   }
+  
   #draw the cat
   cat = "
         |/
  /.\\ /.\\\'
 (= ^.^ =) 
 (\'\'\')(\'\'\')"
+  
   #make the console output with the cat function
   cat(paste0(msg, cat))
   
 }
+
+#alternative function name
+fnc_catSays = f_catSays
